c6b332c2feb98e21d91f28b1d001904a76d5b76f
Dasha Lary <dasha4988@gmail.com>
2021-03-24 15:27:48 -0400

    wrote more methods in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,18 @@ class ApplicationController < ActionController::API
     end
 
     def auth_header
+        request.headers['Authorization']
     end
 
     def decoded_token
-        
+        if auth_header
+            token = auth_header.split(' ')[1]
+            begin
+                JWT.decode(token, 'my_secret', true, algorithm: 'HS256')
+            rescue JWT::DecodeError
+                []
+            end
+        end
     end
 
     def session_user
