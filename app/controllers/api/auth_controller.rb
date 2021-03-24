17dfef9ece74746d810d1d30faa24f8e9f8f4c70
Dasha Lary <dasha4988@gmail.com>
2021-03-24 15:14:23 -0400

    added controller methods

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -1,2 +1,13 @@
 class AuthController < ApplicationController
+    skip_before_action :require_login, only: [:login, :auto_login]
+    def login 
+
+    end
+
+    def auto_login
+    end
+
+    def user_is_authed
+    end
+
 end
