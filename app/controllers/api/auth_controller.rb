e20e468c5f9ada903cd823504ba3a1c4f405d800
Dasha Lary <dasha4988@gmail.com>
2021-03-24 15:21:51 -0400

    more controller methods, auth

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -1,13 +1,27 @@
 class AuthController < ApplicationController
     skip_before_action :require_login, only: [:login, :auto_login]
-    def login 
 
+    def login 
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            payload = {user_id: user.id}
+            token = encode_token(payload)
+            render json: {user: user, jwt: token, success: "Welcome back, #{user.username}!"}
+        else
+            render json: {failure: "Invalid username or password. Unable to log in."}
+        end
     end
 
     def auto_login
+        if session_user
+            render json: session_user
+        else
+            render json: {errors: "You are not logged in."}
+        end
     end
 
     def user_is_authed
+        render json: {message: "You are authorized."}
     end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::API
+    before_action :require_login
+
+    def encode_token(payload)
+    end
+
+    def auth_header
+    end
+
+    def decoded_token
+    end
+
+    def session_user
+    end
+
+    def logged_in?
+        !!session_user
+    end
+
+    def require_login
+        render json: {message: 'You must be logged in to continue.'}, status: :unauthorized unless logged_in?
+    end
 end
