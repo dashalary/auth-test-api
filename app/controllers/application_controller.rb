10bfe9097dfd73bb08a879bc0a6c4f930fa83051
Dasha Lary <dasha4988@gmail.com>
2021-03-24 15:24:53 -0400

    wrote session_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,24 @@ class ApplicationController < ActionController::API
     before_action :require_login
 
     def encode_token(payload)
+        JWT.encode(payload, 'my_secret')
     end
 
     def auth_header
     end
 
     def decoded_token
+        
     end
 
     def session_user
+        decoded_hash = decoded_token
+        if !decoded_hash.empty?
+            user_id = decoded_hash[0]['user_id']
+            @user = User.find_by(id: user_id)
+        else
+            nil 
+        end
     end
 
     def logged_in?
